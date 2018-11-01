142ad1634d79037a4627789c103c16b392cdc846
unknown <mike.p.monahan@gmail.com>
2018-11-01 10:01:39 -0400

    adding mongo endpoints

--- cmds/get-list.js
+++ cmds/get-list.js
@@ -1,1 +1,10 @@
-console.log('hey bro');
+const axios = require('axios');
+const { db } = require('./../config/keys');
+
+(async function (){
+    await axios.get(`https://api.mlab.com/api/1/databases/pictsy-db/collections/images?apiKey=${db.api_key}`).then((data) => {
+        console.log(data.data);
+    }).catch((err) => {
+        console.log('Connection Refused:', err);
+    })
+})()
